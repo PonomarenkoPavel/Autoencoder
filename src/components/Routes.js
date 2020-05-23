0490b636d517e341a5195af71a8545700f1239ed
Pavel <pavel-998@mail.ru>
2020-05-24 05:18:45 +0700

    test

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { TrainNN } from 'containers/TrainNN';
-import {
-  CREATE_NN_URL,
-  TRAIN_NN_URL,
-  TEST_NN_URL,
-  ROOT,
-} from '../constants/api';
-import { CreateNN } from '../containers/CreateNN';
-import { TestNNComponent } from './TestNNComponent';
+// import { Switch, Route } from 'react-router-dom';
+// import { TrainNN } from 'containers/TrainNN';
+// import {
+//   CREATE_NN_URL,
+//   TRAIN_NN_URL,
+//   TEST_NN_URL,
+//   ROOT,
+// } from '../constants/api';
+// import { CreateNN } from '../containers/CreateNN';
+// import { TestNNComponent } from './TestNNComponent';
 import { StartPage } from '../containers/StartPage';
 
-export const Routes = () => (
-  <Switch>
-    <Route path={ROOT} exact component={StartPage} />
-    <Route path={CREATE_NN_URL} exact component={CreateNN} />
-    <Route path={TRAIN_NN_URL} exact component={TrainNN} />
-    <Route path={TEST_NN_URL} exact component={TestNNComponent} />
-  </Switch>
-);
+export const Routes = () => {
+  console.log(123);
+  return (
+    <StartPage />
+    // <Switch>
+    //   <Route path={ROOT} exact component={StartPage} />
+    //   <Route path={CREATE_NN_URL} exact component={CreateNN} />
+    //   <Route path={TRAIN_NN_URL} exact component={TrainNN} />
+    //   <Route path={TEST_NN_URL} exact component={TestNNComponent} />
+    // </Switch>
+  );
+};
