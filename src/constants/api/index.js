84635c99daaa0a53da364b7d30e9af2f5b428470
Pavel <pavel-998@mail.ru>
2020-05-24 05:23:53 +0700

    fix

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -1,6 +1,6 @@
-export const ROOT = '/';
-export const CREATE_NN_URL = '/create';
-export const TRAIN_NN_URL = '/train';
-export const TEST_NN_URL = '/test';
+export const ROOT = 'Autoencoder/';
+export const CREATE_NN_URL = 'Autoencoder/create';
+export const TRAIN_NN_URL = 'Autoencoder/train';
+export const TEST_NN_URL = 'Autoencoder/test';
 export const MNIST_DATA_URL =
   'https://storage.googleapis.com/learnjs-data/model-builder/mnist_images.png';
